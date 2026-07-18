f53c923e5abaf18d97b9fdc7a3e69fa963a1ddf3
tracycrossrvt-gif <tracycrossrvt@gmail.com>
2026-07-18 09:12:28 -0700

    Implement Explore page filtering and load more

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,78 +1,173 @@
 import React from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import CountdownTimer from "../UI/CountdownTimer";
+import Skeleton from "../UI/Skeleton";
 
 const ExploreItems = () => {
+const [items, setItems] = React.useState([]);
+const [visibleItems, setVisibleItems] = React.useState(8);
+const [loading, setLoading] = React.useState(true);
+const [filter, setFilter] = React.useState("");
+
+  React.useEffect(() => {
+  setLoading(true);
+  const url = filter
+    ? `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${filter}`
+    : "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore";
+
+  axios
+    .get(url)
+    .then((response) => {
+      setItems(response.data);
+      setVisibleItems(8);
+    })
+    .catch((error) => {
+      console.error("Unable to fetch explore items:", error);
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+}, [filter]);
+
+    const handleLoadMore = () => {
+  setVisibleItems((currentAmount) => currentAmount + 4);
+};
+
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select
+          id="filter-items"
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+          >
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
+
+      {loading
+  ? new Array(8).fill(0).map((_, index) => (
+      <div
+        key={index}
+        className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+        style={{ display: "block", backgroundSize: "cover" }}
+      >
+        <div className="nft__item">
+          <div className="author_list_pp">
+            <Skeleton
+              width="50px"
+              height="50px"
+              borderRadius="50%"
+            />
+          </div>
+
+          <div className="nft__item_wrap">
+            <Skeleton
+              width="100%"
+              height="250px"
+              borderRadius="6px"
+            />
+          </div>
+
+          <div className="nft__item_info">
+            <Skeleton width="70%" height="20px" />
+            <Skeleton width="40%" height="18px" />
+          </div>
+        </div>
+      </div>
+    ))
+    : items.slice(0, visibleItems).map((item) => (
+    <div
+      key={item.id}
+      className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+      style={{ display: "block", backgroundSize: "cover" }}
+    >
+      <div className="nft__item">
             <div className="author_list_pp">
               <Link
-                to="/author"
+                to={`/author/${item.authorId}`}
                 data-bs-toggle="tooltip"
                 data-bs-placement="top"
               >
-                <img className="lazy" src={AuthorImage} alt="" />
+                <img
+                  className="lazy"
+                  src={item.authorImage}
+                  alt={item.title}
+                />
                 <i className="fa fa-check"></i>
               </Link>
-            </div>
-            <div className="de_countdown">5h 30m 32s</div>
+              </div>
+            
+            {item.expiryDate && item.expiryDate > Date.now() && (
+              <CountdownTimer expiryDate={item.expiryDate} />
+            )}
 
             <div className="nft__item_wrap">
               <div className="nft__item_extra">
                 <div className="nft__item_buttons">
                   <button>Buy Now</button>
+
                   <div className="nft__item_share">
                     <h4>Share</h4>
+
                     <a href="" target="_blank" rel="noreferrer">
                       <i className="fa fa-facebook fa-lg"></i>
                     </a>
+
                     <a href="" target="_blank" rel="noreferrer">
                       <i className="fa fa-twitter fa-lg"></i>
                     </a>
+
                     <a href="">
                       <i className="fa fa-envelope fa-lg"></i>
                     </a>
                   </div>
                 </div>
               </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
+
+              <Link to={`/item-details/${item.nftId}`}>
+                <img
+                  src={item.nftImage}
+                  className="lazy nft__item_preview"
+                  alt={item.title}
+                />
               </Link>
             </div>
+
             <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
+              <Link to={`/item-details/${item.nftId}`}>
+                <h4>{item.title}</h4>
               </Link>
-              <div className="nft__item_price">1.74 ETH</div>
+
+              <div className="nft__item_price">
+                {item.price} ETH
+              </div>
+
               <div className="nft__item_like">
                 <i className="fa fa-heart"></i>
-                <span>69</span>
+                <span>{item.likes}</span>
               </div>
             </div>
           </div>
-        </div>
+          </div>
+      
       ))}
-      <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
-      </div>
+
+        {visibleItems < items.length && (
+  <div className="col-md-12 text-center">
+    <button
+      id="loadmore"
+      className="btn-main lead"
+      onClick={handleLoadMore}
+    >
+      Load more
+    </button>
+  </div>
+)}
     </>
   );
 };
